c01a7ea78aa91531a73dd81df1ae55554d9e1c9a
obanracer <desimone.brunof@gmail.com>
2016-12-22 13:52:58 -0300

    Moved files to repo

---
+++ PongV2/src/main/Game.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.image.BufferStrategy;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Game extends Canvas implements Runnable {
+    
+    private static final int NUMBER_OF_BUFFERS = 2;
+    private static final double TARGET_FPS = 60.0;
+    private static final double NS_PER_UPDATE = 1000000000 / TARGET_FPS;
+    
+    private BufferStrategy bufferStrategy;
+    private boolean running = false;
+    
+    public Game() {
+        init();
+    }
+
+    private void init() {
+        setMinimumSize(new Dimension(Window.WINDOW_WIDTH, Window.WINDOW_HEIGHT));
+        setMaximumSize(new Dimension(Window.WINDOW_WIDTH, Window.WINDOW_HEIGHT));
+        setPreferredSize(new Dimension(Window.WINDOW_WIDTH, Window.WINDOW_HEIGHT));
+        
+        setIgnoreRepaint(true);
+        setFocusable(running);
+        
+        requestFocus();
+    }
+    
+    public synchronized void start() {
+        running = true;
+        Thread thread = new Thread(this);
+        thread.start();
+    }
+    
+    public synchronized void stop() {
+        running = false;
+    }
+    
+    @Override
+    public void run() {
+        while (running) {            
+            System.out.println("running :D");
+        }
+    }
+    
+    public void processInput() {
+        
+    }
+    
+    public void update(double delta) {
+        
+    }
+    
+    public void render() {
+        
+    }
+    
+}
---
+++ PongV2/src/main/Window.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Window extends JFrame {
+    public static final int WINDOW_WIDTH = 800;
+    public static final int WINDOW_HEIGHT = 600;
+    
+    private static final String TITLE = "Pong";
+    private static final String VERSION = "1.0";
+    
+    private Game game;
+    
+    public Window() {
+        init();
+    }
+    
+    private void init() {
+        setTitle(TITLE + " " + VERSION);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        setResizable(false);
+        setIgnoreRepaint(true);
+        
+        game = new Game();
+        add(game, BorderLayout.CENTER);
+        
+        setVisible(true);
+        pack();
+        
+        setLocationRelativeTo(null);
+    }
+    
+    public void startGame() {
+        game.start();
+    }
+}
